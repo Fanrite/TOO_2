b4474c862502b6dd8116478e537546581c782c02
Fanrite <76695059+Fanrite@users.noreply.github.com>
2021-07-04 15:45:15 +0300

    Добавьте файлы проекта.

---
+++ TOO_2/Aircraft.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace TOO_2
+{
+    class Aircraft
+    {
+        private string TypeMachine { get; set; }
+        private int Weight { get; set; }
+        private int AmountEngine { get; set; }
+
+        public Aircraft ()
+        {
+            this.TypeMachine = Interaction.InputBox("Введіть тип літака", "Введення");
+            this.Weight = Convert.ToInt32(Interaction.InputBox("Введіть массу літака", "Введення"));
+            this.AmountEngine = Convert.ToInt32(Interaction.InputBox("Введіть кількість двигунів літака", "Введення"));
+        }
+
+        public Aircraft (string TypeMachine, int Weight, int AmountEngine)
+        {
+            this.TypeMachine = TypeMachine;
+            this.Weight = Weight;
+            this.AmountEngine = AmountEngine;
+        }
+
+        private string GetWeight ()
+        {
+            if (Weight <= 7) return "Легкий";
+            else if (Weight <= 136) return "Середній";
+            else return "Важкий";
+        }
+
+        private string GetEngie ()
+        {
+            return $"{AmountEngine}-Двигунний";
+        }
+
+        public void ShowInfo ()
+        {
+            MessageBox.Show($"Данні літака:\nТип: {TypeMachine}\nВага: {Weight} ({GetWeight()})\nДвигун: {GetEngie()}", "Інформація", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ TOO_2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TOO_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Aircraft Standart = new Aircraft("Транспортний", 10, 2);
+            Standart.ShowInfo();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Aircraft UserCraft = new Aircraft();
+            UserCraft.ShowInfo();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
